b35d82b3afde8ab1ae26fb0eb173d073948e7c2c
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-07 01:52:24 -0300

    Habemus teste unitario verde

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'lib/assets/**/*.js',
+      'lib/assets/vendor/js/foundation/vendor/jquery.js',
+      'lib/assets/vendor/**/*.js',
+      'lib/assets/js/**/*.js',
       'spec/unit/**/*.js'
     ],
 
---
+++ spec/unit/green_test.js
@@ -0,0 +1,5 @@
+describe('A green test', function () {
+  it('should pass', function() {
+    expect(1).toEqual(1);
+  });
+});

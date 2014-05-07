9aef4c58eea4ca146694c37ed0f64fc605efd893
marano <thiagomarano@gmail.com>
2014-05-07 00:49:01 -0300

    Configure grunt watch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,20 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
       dist: {
         src: ['lib/html/index.html'],
         dest: 'public/index.html'
+      },
+      css: {
+        options: {
+          separator: ';',
+        },
+        files: {
+          'public/assets/vendor.css': ['lib/assets/vendor/css/**/*.css'],
+          'public/assets/lib.css': ['lib/assets/css/**/*.css']
+        }
       },
       js: {
         options: {
@@ -15,10 +25,28 @@ module.exports = function (grunt) {
           'public/assets/lib.js': ['lib/assets/js/**/*.js']
         }
       }
+    },
+
+    watch: {
+      styles: {
+        files: ['lib/assets/css/**/*.css'],
+        tasks: ['less', 'concat:css'],
+        options: {
+          nospawn: true
+        }
+      },
+      javascripts: {
+        files: ['lib/assets/**/*.js'],
+        tasks: ['concat:js'],
+        options: {
+          nospawn: true
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['concat']);
 };

f93f9c3c97e79532cc3629ae4fe4c08a1f9195b7
marano <thiagomarano@gmail.com>
2014-05-12 23:18:07 -0300

    Fix functional spec.

--- spec/functional/home_spec.js
+++ spec/functional/home_spec.js
@@ -1,5 +1,5 @@
-describe('Index', function() {
-  it('A valid test', function() {
+describe('Home', function() {
+  it('shows home', function() {
     browser.get('http://tests.deliberare.com.br/');
     var name = element(by.binding('name')).getText();
     expect(name).toEqual('Olá, Deliberare! :)');

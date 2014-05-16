f127837a056ba1fbd5c7afeaf987d952fdcf47a6
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-15 22:23:32 -0300

    Coloca Helpers.site no lugar certo =p

--- spec/unit/helpers/general_site.js
+++ spec/unit/helpers/general_site.js
@@ -1,45 +1,102 @@
-(function(global) {
+'use strict';
 
-  'use strict';
+describe('Helpers.site', function () {
+  var $rootScope, global, utils;
 
-  angular.module('Helpers.site')
+  beforeEach(module('Helpers.site'));
+  beforeEach(inject(function (_$rootScope_, $window, _utils_) {
+    $rootScope = _$rootScope_;
+    global = $window;
+    utils = _utils_;
+  }));
 
-  .run(['$rootScope', '$window', 'User', 'utils', function ($rootScope, $window, User, utils) {
-    var user = User.current();
-    $rootScope.user = user;
+  describe('facebookLogin', function() {
+    it('should login with facebook using Parse API', function() {
+      spyOn(global.Parse.FacebookUtils, 'logIn');
 
-    $rootScope.facebookLogin = function () {
-      global.Parse.FacebookUtils.logIn('public_profile,email,user_location', {
-        success: function (user) {
-          if(!user.existed()) {
-            $window.FB.api('/me', function (fbUserInfo) {
-              var userData = _(fbUserInfo).chain()
-                            .omit('id')
-                            .extend({
-                              facebook_id: fbUserInfo.id,
-                              username   : fbUserInfo.email
-                            })
-                            .value();
+      $rootScope.facebookLogin();
 
-              user.save(userData).then(function(user) {
-                $rootScope.user = user;
-                utils.redirect('/');
-              });
+      expect(global.Parse.FacebookUtils.logIn).toHaveBeenCalled();
+    });
+
+    it('should ask for permissions: public_profile, email and user_location', function() {
+      spyOn(global.Parse.FacebookUtils, 'logIn');
+
+      $rootScope.facebookLogin();
+
+      expect(global.Parse.FacebookUtils.logIn).toHaveBeenCalledWith('public_profile,email,user_location', jasmine.any(Object));
+    });
+
+    describe('after login', function() {
+      beforeEach(function() {
+        global.FB = {
+          api: function(url, callback) {
+            callback({
+              id: 'facebookId',
+              email: 'userEmail'
             });
-          } else {
-            utils.redirect('/');
           }
-        }
+        };
+
+        spyOn(global.Parse.User.prototype, 'save').andReturn({
+          then: function(callback) { callback(); }
+        });
+
+        spyOn(utils, 'redirect');
       });
-    };
-  }])
 
-  .factory('utils', ['$window', function(global) {
-    return {
-      redirect: function(url) {
-        global.location.href = url;
-      }
-    };
-  }]);
+      it("should save the user only if it doesn't exists", function() {
+        spyOn(global.Parse.FacebookUtils, 'logIn').andCallFake(function (permissions, callbacks) {
+          var user = new global.Parse.User();
+          user.existed = function() {
+            return true;
+          };
+          callbacks.success(user);
+        });
 
-}(window));
+        $rootScope.facebookLogin();
+
+        expect(global.Parse.User.prototype.save).not.toHaveBeenCalled();
+      });
+
+      it("should save the user by passing facebook's info plus: facebook_id instead of the id, username as user's email", function() {
+        spyOn(global.Parse.FacebookUtils, 'logIn').andCallFake(function (permissions, callbacks) {
+          callbacks.success(new global.Parse.User());
+        });
+
+        $rootScope.facebookLogin();
+
+        expect(global.Parse.User.prototype.save).toHaveBeenCalledWith({
+          facebook_id: 'facebookId',
+          username: 'userEmail',
+          email: 'userEmail'
+        });
+      });
+
+      it('should redirect to root if it exists', function() {
+        spyOn(global.Parse.FacebookUtils, 'logIn').andCallFake(function (permissions, callbacks) {
+          callbacks.success(new global.Parse.User());
+        });
+
+        $rootScope.facebookLogin();
+
+        expect(utils.redirect).toHaveBeenCalledWith('/');
+      });
+
+      it("should redirect to root if doesn't exists", function() {
+        spyOn(global.Parse.FacebookUtils, 'logIn').andCallFake(function (permissions, callbacks) {
+          var user = new global.Parse.User();
+          user.existed = function() {
+            return true;
+          };
+          callbacks.success(user);
+        });
+
+        $rootScope.facebookLogin();
+
+        expect(utils.redirect).toHaveBeenCalledWith('/');
+      });
+    });
+  });
+
+});

3dfbb12ec4921383a905fd586516f914ea328710
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-15 23:38:45 -0300

    Teste para o modulo Helpers

---
+++ spec/unit/helpers/general.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Helpers', function () {
+  var User, $rootScope, assignVars;
+
+  beforeEach(module('Helpers'));
+  beforeEach(inject(function(_$rootScope_, _User_, _assignVars_) {
+    $rootScope = _$rootScope_;
+    User = _User_;
+    assignVars = _assignVars_;
+  }));
+
+  it('should assign a user with the current logged user', function() {
+    spyOn(User, 'current').andReturn('theUser');
+
+    assignVars();
+
+    expect($rootScope.user).toEqual('theUser');
+  });
+
+
+});

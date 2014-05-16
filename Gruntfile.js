988e7a162bcc5af1112b1bd8a20d64af1e4f4d1c
Carlos Villela <cvillela@thoughtworks.com>
2014-05-16 14:37:12 -0300

    Builds source maps

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,8 +90,11 @@ module.exports = function (grunt) {
 
     uglify: {
       options: {
-        mangle: false
+        mangle: false,
+        sourceMap: true,
+        sourceMapIncludeSources: true
       },
+
       vendor_js: {
         options: {
           separator: '\n;',

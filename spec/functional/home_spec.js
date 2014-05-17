ac65f89e2206c9e9329ca2781d9e2e1f83cf608b
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-16 22:05:43 -0300

    Conserta os testes funcionais

--- spec/functional/home_spec.js
+++ spec/functional/home_spec.js
@@ -2,8 +2,8 @@
 
 describe('Home', function() {
   it('shows link for user to login', function() {
-    browser.get('http://tests.deliberare.com.br/');
-    var command = element(by.css('.user-area')).getText();
+    browser.get('http://local.deliberare.com.br:8000/');
+    var command = element(by.css('[test-name="login-button"]')).getText();
     expect(command).toEqual('Entrar');
   });
 });

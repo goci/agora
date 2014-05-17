7f7507ade121ad6ac62ef35dcd82fd15739115dd
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-16 22:50:14 -0300

    Muda a url acessada nos testes para apenas '/'

--- spec/functional/home_spec.js
+++ spec/functional/home_spec.js
@@ -2,7 +2,7 @@
 
 describe('Home', function() {
   it('shows link for user to login', function() {
-    browser.get('http://local.deliberare.com.br:8000/');
+    browser.get('/');
     var command = element(by.css('[test-name="login-button"]')).getText();
     expect(command).toEqual('Entrar');
   });

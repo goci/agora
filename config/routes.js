ff1cccf7e2ddc694ea8d5f26f16dde2c033f0052
Rafael Garcia <rafbgarcia@gmail.com>
2014-06-04 22:56:30 -0300

    Padroniza rotas

--- config/routes.js
+++ config/routes.js
@@ -3,37 +3,56 @@
 
   angular.module('Routes')
 
-  .config(['$stateProvider', '$urlRouterProvider',
-    function ($stateProvider, $urlRouterProvider) {
+  .constant('states', [
+    'user_edit',
+    'communities_index' // { url: '/communities', controller: 'CommunitiesIndex', templateUrl: '/communities/index.tmpl.html' }
+  ])
+
+  .config(['$stateProvider', '$urlRouterProvider', 'states',
+    function ($stateProvider, $urlRouterProvider, states) {
       $urlRouterProvider.otherwise("/");
 
       $stateProvider
+        .state('root', {
+          url: "/",
+          templateUrl: 'home/index.tmpl.html'
+        })
+        .state('logout', {
+          url: "/logout",
+          controller: function (User, $state, $rootScope) {
+            User.logOut();
+            $rootScope.user = undefined;
+            $state.go('root');
+          }
+        });
 
-      .state('root', {
-        url: "/",
-        templateUrl: 'home/index.tmpl.html'
-      })
-
-      .state('logout', {
-        url: "/logout",
-        controller: function (User, $state, $rootScope) {
-          User.logOut();
-          $rootScope.user = undefined;
-          $state.go('root');
-        }
-      })
-
-      .state('editUser', {
-        url: '/user/edit',
-        controller: 'UserEdit',
-        templateUrl: 'user/edit.tmpl.html'
-      })
-
-      .state('communities', {
-        url: '/communities',
-        controller: 'CommunityIndex',
-        templateUrl: 'community/index.tmpl.html'
+      states.forEach(function (state) {
+        $stateProvider.state(state, {
+          url: urlForState(state),
+          controller: controllerForState(state),
+          templateUrl: templateForState(state)
+        });
       });
+
+      function urlForState(state) {
+        var pieces = state.split('_');
+        var path = pieces.join('/');
+        return '/' + path.replace(/\/index$/, '');
+      }
+
+      function controllerForState(state) {
+        var controller = '';
+        state.split('_').forEach(function (piece) {
+          controller += piece[0].toUpperCase() + piece.substring(1, piece.length);
+        });
+        return controller;
+      }
+
+      function templateForState(state) {
+        var pieces = state.split('_');
+        var path = pieces.join('/');
+        return path + '.tmpl.html';
+      }
     }
   ]).run(['$state', _.noop]); // re-evaluates routes when page is reloaded
 }());

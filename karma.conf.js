07cfafd0726d3a2cf30324e2434b2d917a8fb8cf
marano <thiagomarano@gmail.com>
2014-06-06 11:03:15 -0300

    Should not need to use the sandbox flag when running karma.

--- karma.conf.js
+++ karma.conf.js
@@ -60,15 +60,7 @@ module.exports = function (config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['ChromeNoSandbox'],
-
-    customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
-
+    browsers: ['Chrome'],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
